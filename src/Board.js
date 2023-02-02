408f486fc5f66c0fcdd7f37b3a7e6c65d4df5606
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-02-02 18:24:51 -0300

    limpa e faz um hello world

---
+++ src/Board.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import "./Board.css";
+
+function Board() {
+  return (
+    <>
+      <h2>Dentro do componente Board</h2>
+    </>
+  );
+}
+
+export default Board;
